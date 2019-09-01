72762c07609b616ec291eb71bd698ca170420a6b
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-31 22:55:35 -0700

    finishes callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -38,27 +38,54 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-
+// ====================================================
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
+console.log(
+  getLength(items, length => `The length of the array is ${length}.`)
+);
+
+// ====================================================
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
 
+console.log(last(items, last => `The last item is ${last}.`));
+
+// ====================================================
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let result = x + y;
+  return cb(result);
 }
 
+console.log(sumNums(4, 5, sum => `The sum is ${sum}.`));
+
+// ====================================================
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let result = x * y;
+  return cb(result);
 }
 
+console.log(multiplyNums(2, 3, product => `The product is ${product}.`));
+
+// ====================================================
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+    return cb(true);
+  } else {
+    return cb(false);
+  }
 }
+
+console.log(contains("Gum", items, cb => cb));
 
 /* STRETCH PROBLEM */
 
